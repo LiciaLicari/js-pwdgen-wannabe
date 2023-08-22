9698fb25729b36a5913f5b1992bdc56890f2092e
“LiciaLicari” <licianbsplicari@gmail.com>
2023-08-22 14:12:19 +0200

    Initial Commit

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+/* 
+
+consegna: Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato,
+che va formattato in questo modo: nomecognomecolorepreferito23 
+Esempio: fabiopacificiblue23 
+
+
+Strumenti:
+        prompt
+        const
+        getElementById
+        console.log
+
+    Pseudo Codice:
+        - Richiedo all'user il suo nome
+        - Richiedo all'user il suo cognome
+        - Richiedo all'user il suo colore preferito
+        - Genero la psw concatenando le stringhe. Aggiungo '23' alla fine
+        - Scrivo il risultato in console
+        - Scrivo il srisultato in pagina
+*/ 
+
